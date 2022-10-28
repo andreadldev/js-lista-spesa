f9e5c64bbb5b58dc5c14c0e4a77099bb89b1faca
andreadldev <andrea.delisi2@gmail.com>
2022-10-28 09:51:30 +0200

    Sposta costante fuori dal ciclo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("list");
 const shoppingList = ["Pane", "Latte", "Uova", "Caffè", "Pasta"];
+const listContainer = document.getElementById("list");
 const lastItem = shoppingList[shoppingList.length - 0];
 
 let done = false;
@@ -7,7 +8,6 @@ let i = 0;
 
 while (done != true) {
     console.log(shoppingList[i]);
-    const listContainer = document.getElementById("list");
     const listItem = document.createElement("li");
     listItem.innerHTML = shoppingList[i];
     listContainer.append(listItem);
